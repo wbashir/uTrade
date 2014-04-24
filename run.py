1e5ad149ac40893ce9b399bafb7a91e1f95e7aa4
Warsame Bashir <warsameb0@gmail.com>
2014-04-23 21:25:45 -0500

    Revamping - Initial commit

--- run.py
+++ run.py
@@ -1,8 +1,79 @@
-#!/usr/bin/env python
-from app import app
+#----------------------------------------------------------------------------#
+# Imports.
+#----------------------------------------------------------------------------#
 
-app.run(
-    host=app.config.get('HOST'),
-    port=app.config.get('PORT'),
-    debug=app.config.get('DEBUG')
-)
+from flask import * # do not use '*'; actually input the dependencies.
+from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app.config.from_object('config')
+#db = SQLAlchemy(app)
+
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+@app.route('/')
+def home():
+    return render_template('pages/home.html')
+
+
+# Error handlers.
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 requirejs.config({
   baseUrl: "/static/",
   paths: {
-    flight: "bower_components/flight/",
     lodash: "bower_components/lodash/dist/lodash.min",
 
     ui: "js/ui",
