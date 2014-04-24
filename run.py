69216b9cea727861f560324bc1a41a17bdbf59ec
Warsame Bashir <warsameb0@gmail.com>
2014-04-23 22:12:43 -0500

    Basic layout  update.

--- run.py
+++ run.py
@@ -22,18 +22,7 @@ def shutdown_session(exception=None):
     db_session.remove()
 '''
 
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
+
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
